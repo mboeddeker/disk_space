6cb0baae969cad3fdc59ac533f586ffe92ff6e6a
phipps980316 <59151421+phipps980316@users.noreply.github.com>
2021-06-25 19:00:23 +0100

    switch to embedding v2

--- android/src/main/kotlin/de/appgewaltig/disk_space/DiskSpacePlugin.kt
+++ android/src/main/kotlin/de/appgewaltig/disk_space/DiskSpacePlugin.kt
@@ -2,42 +2,44 @@ package de.appgewaltig.disk_space
 
 import android.os.Environment
 import android.os.StatFs
+import io.flutter.embedding.engine.plugins.FlutterPlugin
 import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
-import io.flutter.plugin.common.PluginRegistry.Registrar
 
-class DiskSpacePlugin: MethodCallHandler {
-  companion object {
-    @JvmStatic
-    fun registerWith(registrar: Registrar) {
-      val channel = MethodChannel(registrar.messenger(), "disk_space")
-      channel.setMethodCallHandler(DiskSpacePlugin())
-    }
+class DiskSpacePlugin: FlutterPlugin, MethodCallHandler {
+
+  private var channel : MethodChannel? = null
+
+  override fun onAttachedToEngine(binding: FlutterPlugin.FlutterPluginBinding) {
+    channel = MethodChannel(binding.binaryMessenger, "disk_space")
+    channel!!.setMethodCallHandler(this)
+  }
+
+  override fun onDetachedFromEngine(binding: FlutterPlugin.FlutterPluginBinding) {
+    channel = null
   }
 
   private fun getFreeDiskSpace(): Double {
     val stat = StatFs(Environment.getExternalStorageDirectory().path)
 
-    var bytesAvailable: Long
-
-    if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.JELLY_BEAN_MR2)
-      bytesAvailable = stat.blockSizeLong * stat.availableBlocksLong
+    val bytesAvailable: Long
+    bytesAvailable = if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.JELLY_BEAN_MR2)
+      stat.blockSizeLong * stat.availableBlocksLong
     else
-      bytesAvailable = stat.blockSize.toLong() * stat.availableBlocks.toLong()
+      stat.blockSize.toLong() * stat.availableBlocks.toLong()
     return (bytesAvailable / (1024f * 1024f)).toDouble()
   }
 
   private fun getTotalDiskSpace(): Double {
     val stat = StatFs(Environment.getExternalStorageDirectory().path)
 
-    var bytesAvailable: Long
-
-    if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.JELLY_BEAN_MR2)
-      bytesAvailable = stat.blockSizeLong * stat.blockCountLong
+    val bytesAvailable: Long
+    bytesAvailable = if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.JELLY_BEAN_MR2)
+      stat.blockSizeLong * stat.blockCountLong
     else
-      bytesAvailable = stat.blockSize.toLong() * stat.blockCount.toLong()
+      stat.blockSize.toLong() * stat.blockCount.toLong()
     return (bytesAvailable / (1024f * 1024f)).toDouble()
   }
 
